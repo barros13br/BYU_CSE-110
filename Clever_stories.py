f606d57ddd489451f20444d64fea8fa7ea62cd91
Antonio Barros <106938712+barros13br@users.noreply.github.com>
2025-10-31 13:31:54 -0300

    REFAZENDO O CLEVER STORIES

---
+++ Clever_stories.py
@@ -0,0 +1,22 @@
+#create a story using words that the user entered in input
+print("Please enter the following:")
+print()
+
+#ask to user put the information
+adjective = input("An adjective: ")
+animal = input("An animal: ")
+verb_1 = input("A verb: ")
+exclamation = input("An exclamation: ")
+verb_2 = input("Anothoer verb: ")
+verb_3 = input("One more verb: ")
+adjective_2 = input("Enter other adjective: ")
+verb_4 = input("A verb again: ")
+adjective_3 = input("One more adjective: ")
+verb_5 = input ("Another verb again: ")
+verb_6 = input ("And the last one: ")
+print()
+
+#make the story
+print(f'The other day, I was really in trouble. It all started when I saw a very {adjective} {animal.capitalize()} {verb_1} down the hallway. "{exclamation.capitalize()}!" I yelled. But all I could think to do was to {verb_2} over and over. Miraculously, that caused it to stop, but not before it tried to {verb_3} right in front of my family.')
+print(f'Everyone was so {adjective_2} when it started to {verb_4} around the room.
+Then my {adjective_3} neighbor came in and tried to {verb_5} it, but ended up {verb_6} instead.')
